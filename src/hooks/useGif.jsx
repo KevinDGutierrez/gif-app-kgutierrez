55fc95ea1423fd618604003c23356188d504b0ff
KevinDGutierrez <transgutis01@gmail.com>
2026-01-22 14:55:26 -0600

    Funcionalidad de generar gif

--- src/hooks/useGif.jsx
+++ src/hooks/useGif.jsx
@@ -5,7 +5,7 @@ export const useGif = () => {
     const [gif, setGif] = useState('')
 
     const reqGif = async() =>{
-        await fetch('http://api.giphy.com/v1/gifs/random?api_key=ryCT6STULMGR8J2WlJ4gV6eeX3KGNhae').then(async(resp) =>{
+        await fetch('https://api.giphy.com/v1/gifs/random?api_key=ryCT6STULMGR8J2WlJ4gV6eeX3KGNhae').then(async(resp) =>{
             await resp.json().then(({data})=>{
                 setGif(data.images.original.url)
             })
